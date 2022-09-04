d7f37029909fd31992a936b3cebcb93e55ac65e5
Vladislav Vavilikhin <vavilikhin.v@gmail.com>
2022-09-04 16:19:31 +0300

    fix: pre-commit hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,34 @@
-import type { NextPage } from "next";
-import { useSession, signIn, signOut } from 'next-auth/react'
+import type { NextPage } from 'next';
+import { useSession, signIn, signOut } from 'next-auth/react';
 import { useEffect } from 'react';
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession()
+    const { data: session, status } = useSession();
 
-  useEffect(() => {
-    console.debug(status)
-  }, [status])
+    useEffect(() => {
+        console.debug(status);
+    }, [status]);
 
-
-  return (
-    <div>
-      {session ? (
-        <>
-          <p>You are logged in as {session.user?.email}</p>
-          <button onClick={() => signOut()}> Sign Out</button>
-        </>
-      ) : (
-        <>
-          <p>Who are you?</p>
-          <button style={{ backgroundColor: 'lightgrey' }} onClick={() => signIn()}> Sign In</button>
-        </>
-      )}
-    </div>
-
-  )
+    return (
+        <div>
+            {session ? (
+                <>
+                    <p>You are logged in as {session.user?.email}</p>
+                    <button onClick={() => signOut()}> Sign Out</button>
+                </>
+            ) : (
+                <>
+                    <p>Who are you?</p>
+                    <button
+                        style={{ backgroundColor: 'lightgrey' }}
+                        onClick={() => signIn()}>
+                        {' '}
+                        Sign In
+                    </button>
+                </>
+            )}
+        </div>
+    );
 };
-
 
 export default Home;
