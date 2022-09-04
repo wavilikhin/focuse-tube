8f256a6dc2f0f6cda87cd6225ca2814bcaaca11c
Vladislav Vavilikhin <vavilikhin.v@gmail.com>
2022-09-04 15:39:21 +0300

    feat: prettier and eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,8 +20,14 @@ const MyApp: AppType = ({
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  // browser should use relative url
+  if (typeof window !== "undefined") return "";
+
+  // SSR should use vercel url
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  };
+
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
